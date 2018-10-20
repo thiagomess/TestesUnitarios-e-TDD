fd9a85226507f89f2a04f043ff81ebc07c2c0cc4
thiagomess <thiagogomes19@hotmail.com>
2018-10-20 19:30:57 -0300

    7. AssertThat

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,7 +1,12 @@
 package br.ce.wcaquino.servicos;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
 import java.util.Date;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,6 +16,7 @@ import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
+	//DOC. HAMCREST http://hamcrest.org/JavaHamcrest/javadoc/1.3/index.html?help-doc.html
 	
 	@Test
 	public void teste() {
@@ -25,8 +31,18 @@ public class LocacaoServiceTest {
 		Locacao locacao = service.alugarFilme(usuario, filme);
 
 		// verificacao
-		Assert.assertEquals(4.0, locacao.getValor(), 0.01);
+/*		Assert.assertEquals(4.0, locacao.getValor(), 0.01);
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+*/
+		
+        Assert.assertThat(locacao.getValor(), CoreMatchers.is(4.0)); //forma completa 
+		assertThat(locacao.getValor(), is(4.0)); //forma deixando static os imports (utilizando ctrl + shift M)
+		
+		Assert.assertThat(locacao.getValor(), is(CoreMatchers.not(5.0)));//forma completa negando a afirmacao
+		assertThat(locacao.getValor(), is(not(5.0)));//forma deixando static os imports (utilizando ctrl + shift M), negando a afirmacao
+		
+		Assert.assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true)); 
+		Assert.assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), CoreMatchers.is(true));
 	}
 }
