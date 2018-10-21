55fd277d94b5f788171f49df8d648b906fa84eaf
thiagomess <thiagogomes19@hotmail.com>
2018-10-21 17:17:44 -0200

    13. Desafio para adicionar Lista de filmes e refatorar os testes

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -2,7 +2,9 @@ package br.ce.wcaquino.servicos;
 
 import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -13,21 +15,29 @@ import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
 
-	public Locacao alugarFilme(Usuario usuario, Filme filme) throws LocadoraException, FilmeSemEstoqueException {
+	public Locacao alugarFilme(Usuario usuario, List<Filme> filmes) throws LocadoraException, FilmeSemEstoqueException {
 		if (usuario == null) {
 			throw new LocadoraException("Usuario vazio");
 		}
-		if (filme == null) {
+		if (filmes == null) {
 			throw new LocadoraException("Filme vazio");
 		}
-		if (filme.getEstoque() == 0) {
-			throw new FilmeSemEstoqueException();
-		} 
+
+		for (Filme filme : filmes) {
+			if (filme.getEstoque() == 0) {
+				throw new FilmeSemEstoqueException();
+			}
+		}
+
 		Locacao locacao = new Locacao();
-		locacao.setFilme(filme);
+		locacao.setFilmes(filmes);
 		locacao.setUsuario(usuario);
 		locacao.setDataLocacao(new Date());
-		locacao.setValor(filme.getPrecoLocacao());
+		Double valorTotal = 0d;
+		for (Filme filme : filmes) {
+			valorTotal += filme.getPrecoLocacao();
+		}
+		locacao.setValor(valorTotal);
 
 		// Entrega no dia seguinte
 		Date dataEntrega = new Date();
@@ -38,18 +48,19 @@ public class LocacaoService {
 		// TODO adicionar método para salvar
 
 		return locacao;
+
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		// Teste Manual sem framework
 
 		// cenario
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
-		Filme filme = new Filme("Filme 1", 2, 4.0);
+		List<Filme> filmes =  Arrays.asList(new Filme("Filme 1", 2, 4.0));
 
 		// acao
-		Locacao locacao = service.alugarFilme(usuario, filme);
+		Locacao locacao = service.alugarFilme(usuario, filmes);
 
 		// verificacao
 		System.out.println(locacao.getValor() == 4.0);
@@ -57,5 +68,5 @@ public class LocacaoService {
 		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 
 	}
-	
+
 }
--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,9 +1,15 @@
 package br.ce.wcaquino.servicos;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
@@ -19,33 +25,59 @@ import br.ce.wcaquino.utils.DataUtils;
 public class LocacaoServiceTest {
 	//DOC. HAMCREST http://hamcrest.org/JavaHamcrest/javadoc/1.3/index.html?help-doc.html
 	
+	private LocacaoService service;
+	
+	private static Integer numero = 0; //desse modo a variavel nao é reinicializada a cada teste
+	
 	@Rule
 	public ErrorCollector error = new ErrorCollector();
 	
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
 	
+	@Before
+	public void setup() {
+		System.out.println("before");
+		service = new LocacaoService();
+		numero++;
+		System.out.println("contador: "+numero);
+		
+	}
+	@After
+	public void tearDown() {
+		System.out.println("after");
+	}
+	@BeforeClass
+	public static void setupClas() {
+		System.out.println("before class");
+	}
+	@AfterClass
+	public static void tearDownClass() {
+		System.out.println("after class");
+	}
+	
+	
 	@Test
 	public void testeLocacao() throws Exception {
 		// Teste com Junit de condicao
-
+		
 		// cenario
-		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
-		Filme filme = new Filme("Filme 1", 2, 4.0);
-
+		List<Filme> filmes =  Arrays.asList(new Filme("Filme 1", 2, 4.0), new Filme("Filme 2", 2, 4.0));
+		
 		// acao
-		Locacao locacao = service.alugarFilme(usuario, filme);
-
+		Locacao locacao = service.alugarFilme(usuario, filmes);
+		
 		// verificacao
 /*		Assert.assertEquals(4.0, locacao.getValor(), 0.01);
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 */
 		
-        error.checkThat(locacao.getValor(), CoreMatchers.is(4.0)); //forma completa 
+        error.checkThat(locacao.getValor(), CoreMatchers.is(8.0)); //forma completa 
         error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true)); 
         error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), CoreMatchers.is(true));
+        System.out.println("teste");
 	}
 	
 	
@@ -53,27 +85,23 @@ public class LocacaoServiceTest {
     //METODO 1 PARA  QND APENAS A EXCECAO FOR IMPORTANTE
     @Test(expected=FilmeSemEstoqueException.class) //Captura a exception aqui
 	public void testeLocacao_filmeSemEstoque() throws Exception {
-
 		// cenario
-		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
-		Filme filme = new Filme("Filme 1", 0, 4.0);
+		List<Filme> filmes =  Arrays.asList(new Filme("Filme 1", 0, 4.0));
 
 		// acao
-		service.alugarFilme(usuario, filme);
+		service.alugarFilme(usuario, filmes);
 	}	
 	
     //METODO 2, FORMA MAIS ROBUSTA E PRINCIPALMENTE QND A MSG É IMPORTANTE
 	@Test
 	public void testeLocacao_usuarioVazio() throws FilmeSemEstoqueException {
-
 		// cenario
-		LocacaoService service = new LocacaoService();
-		Filme filme = new Filme("Filme 1", 2, 4.0);
+		List<Filme> filmes =  Arrays.asList(new Filme("Filme 1", 2, 4.0));
 
 		// acao
 		try {
-			service.alugarFilme(null, filme);
+			service.alugarFilme(null, filmes);
 			Assert.fail("Deveria lançar excpetion"); // Para evitar falso positivo coloca como fail, 
                                                         //caso nao de fail na linha acima
 		} catch (LocadoraException e) {
@@ -82,12 +110,10 @@ public class LocacaoServiceTest {
 		}
 	}	
 
-    //METODO 2, FORMA NOVA IDEAL QND A EXCEPTION E A MSG SAO IMPORTANTES
+    //METODO 3, FORMA NOVA IDEAL QND A EXCEPTION E A MSG SAO IMPORTANTES
    	@Test
 	public void testeLocacao_filmeVazio() throws FilmeSemEstoqueException, LocadoraException {
-
 		// cenario
-		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
 
 		exception.expect(LocadoraException.class); //captura a exception
